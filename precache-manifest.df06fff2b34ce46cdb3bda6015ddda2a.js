7cc916b3b9ec60a0a847aae6c10fd686a4c74725
grounddemon <thegrounddemon@gmail.com>
2019-07-22 10:37:53 -0700

    Updates

---
+++ precache-manifest.df06fff2b34ce46cdb3bda6015ddda2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c7657ad1614da87f18e1998398ca39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "195f5f57ed2dc7b422b6",
+    "url": "/static/css/main.b03753f5.chunk.css"
+  },
+  {
+    "revision": "acb1e593a286f7483e81",
+    "url": "/static/js/2.fd5e88f8.chunk.js"
+  },
+  {
+    "revision": "195f5f57ed2dc7b422b6",
+    "url": "/static/js/main.f380cc71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b072dcff68b7e58adab1043a6d2157a",
+    "url": "/static/media/Lato-Black.3b072dcf.ttf"
+  },
+  {
+    "revision": "c5eb54404c9dbda925f7142d6343c913",
+    "url": "/static/media/Lato-Regular.c5eb5440.ttf"
+  },
+  {
+    "revision": "bf3ac7b2d11cb28a3411c2c6258b1843",
+    "url": "/static/media/crack_in_ground.bf3ac7b2.png"
+  },
+  {
+    "revision": "37b83b518472198a109fb4eecb18cf9d",
+    "url": "/static/media/polygon.37b83b51.png"
+  }
+]);
