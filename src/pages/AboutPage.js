bdfc5cac4d46a7c29d76e6a0f6a6d48f617fdb35
grounddemon <thegrounddemon@gmail.com>
2019-07-24 13:57:31 -0700

    assets

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,7 +10,29 @@ Rising Beneath You (RBY) is an investment program committed to making as much mo
 		
 			<br/><br/>
 In July, 2019 we began posting our trades in real time on Twitter. 
-
+	<br/><br/>
+RBY Principles:
+<ul>
+<li>Invest with 5 year time horizions.</li>
+<li>Hold no more than 8 assets.</li>
+<li>Companies should have the following charactoristics:</li>
+<ul>
+<li>Leaders of secular growth stories. </li>
+<li>Ability to innovate. (Discover new opportunities and executed better than anyone else)</li>
+<li>Customer obssessed. (Everything is done based off customer needs)</li>
+<li>Technological advantages (Proprietry technology works better than anyone elses).</li>
+<li>Network effects (the more people using the product the more useful it is).</li>
+<li>Economies at scale (cost savings gained by producing something on a large scale).</li>
+<li>Strong branding that can't be replicated.</li>
+</ul>
+<li>Must have admiration and trust for founding CEO. </li>
+<li>If founding CEO is replaced, find another opportunity.</li>
+<li>Identify bad conferences calls and sell. There are other opportunties.</li>
+<li>Wait for IPO's to dip before starting a position. If they go up find another opportunity.</li>
+<li>Be ready for corrections and buy great companies on the way down.</li>
+<li>Never buy on the way up. Be patient.</li>
+<li>Always be 50% invested in U.S companies.</li>
+</ul>
 
 			</span>
 			</div> 
