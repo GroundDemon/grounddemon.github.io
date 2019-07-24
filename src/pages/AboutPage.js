fce67365139160d08b1d56d5caac4714e049970a
grounddemon <thegrounddemon@gmail.com>
2019-07-23 17:24:22 -0700

    assets

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -6,7 +6,7 @@ export default function AboutPage() {
 		<div className="Page-Title">
 			<span className="Bold-Font">About</span> <br/>
 			<span className="Paragraph-Font">
-Rising Beneath You (RBY) is an investment program committed to making as much money as possible. In 2017 we started project Ground Demon with the goal to turn $100K into $1MM. Since then the project's outperformed the market by 300% (41% YOY), and its total value is >$500k.
+Rising Beneath You (RBY) is an investment program committed to making as much money as possible. In 2017 we started project Ground Demon with the goal to turn $100K into $1MM. Since then the project's outperformed the market by 300% (41% average annual return), and its total value is >$500k.
 		
 			<br/><br/>
 In July, 2019 we began posting our trades in real time on Twitter. 
